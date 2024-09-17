961bc435b2466449a1006f0d6b1ec3f57b584b82
April Ray <aprilray1@gmail.com>
2024-09-16 23:17:36 -0400

    Updated url info

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const apiKey = process.env.NEWS_API_KEY;
-const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`
+let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`
 
 async function fetchNews() {
     try {
@@ -8,14 +8,21 @@ async function fetchNews() {
       console.log(data);
       displayNews(data.articles);
     } catch (error) {
-      console.error('There was an error!', error);
+      console.error(error);
     }
   }
   
   function displayNews(articles) {
     const newsDiv = document.querySelector('#news');
+    newsDiv.innerHTML = '';
+
+    const gridContainer = document.createElement('div');
+    gridContainer.className = 'grid-container';
+    newsDiv.appendChild(gridContainer);
+
     for (const article of articles) {
         const articleDiv = document.createElement('div');
+        articleDiv.className = 'article';
 
         const publishedAt = document.createElement('h4');
         publishedAt.textContent = article.publishedAt;
@@ -29,11 +36,13 @@ async function fetchNews() {
         description.textContent = article.description;
         articleDiv.appendChild(description);
   
-        const url = document.createElement('h6');
-        url.textContent = article.url;
-        articleDiv.appendChild(url);
+        const link = document.createElement('a');
+        link.href = article.url;
+        link.target = '_blank';
+        link.textContent = "Click to read more";
+        articleDiv.appendChild(link);
 
-        newsDiv.appendChild(articleDiv);
+        gridContainer.appendChild(articleDiv);
     }
 }
 
